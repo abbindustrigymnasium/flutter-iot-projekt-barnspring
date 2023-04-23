9a9a8a62aa37f4c06a3fb1df7467e58e250bf9f0
QXPP <Felix.folkebrant@gmail.com>
2023-04-23 15:40:48 +0200

    Allt är klart

--- App.jsx
+++ App.jsx
@@ -1,9 +1,42 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Button, Text } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, TouchableOpacity, Button, Text, Dimensions  } from 'react-native';
 import ProgressCircle from './progressCircle.jsx';
 import { useFonts } from 'expo-font';
+import Swiper from 'react-native-swiper'
+import { SvgXml } from 'react-native-svg';
+import * as Progress from 'react-native-progress';
+
+import allChallenges from './test_challenges.json';
+
+
+import DashboardSvg from './assets/svg/Dashboard.svg';
+import StatsSvg from './assets/svg/Stats.svg';
+import MapSvg from './assets/svg/Map.svg';
+import ProfileSvg from './assets/svg/Profile.svg';
+
+import Map2Svg from './assets/svg/Map2.svg';
+import StepsSvg from './assets/svg/Steps.svg';
+import SpeedSvg from './assets/svg/Speed.svg';
+
 
 const App = () => {
+
+  const { width } = Dimensions.get('window');
+  const swiperRef = useRef(null);
+  const [currentPageIndex, setCurrentPageIndex] = useState(0);
+
+  const topChallenges = allChallenges.sort((a, b) => {
+    return b.progress / b.goal - a.progress / a.goal;
+  }).slice(0, 3);
+
+  // const [allChallenges, setChallenges] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('./test_challenges.json')
+  //     .then((response) => response.json())
+  //     .then((json) => setChallenges(json))
+  //     .catch((error) => console.error(error));
+  // }, []);
 
   const shadowStyle = {
     shadowColor: '#0085FF',
@@ -33,39 +66,111 @@ const App = () => {
   }
   return (
     <View className="w-full h-full flex flex-col items-center bg-[#0A0A1C]">
-      <View className="w-11/12 mt-10 flex items-start">
-        <Text className="text-white text-3xl font-[PM] font-semibold">
-          Dashboard
-        </Text>
-      <View className="w-full flex justify-center items-center">
-        <View className="bg-[#10102C] mt-6 flex justify-center items-center w-[95%] h-[300px] rounded-3xl border-[#20204C] border">
-          <View className=" w-full h-5/6 flex items-center justify-between">
-            <Text className="text-white text-xl font-[PM]">
-              Todays Goal
+      <Swiper
+          paginationStyle={{
+            bottom: -1000
+          }}
+          loop={false}
+          ref={swiperRef}
+          onIndexChanged={(e) => setCurrentPageIndex(e)}
+        >
+          {/* Start of page 0 */}
+          <View className="w-full mt-20 flex items-center">
+            <View className="w-11/12">
+            <Text className="text-white text-3xl font-[PM] font-semibold">
+              Dashboard
             </Text>
-            <View className="relative flex justify-center items-center w-36 h-44">
-              <View className=" bg-LB w-[120px] h-[120px] absolute rounded-full">
-              </View>
-            <View className="absolute flex justify-center items-center">
-              <Text className="text-white text-[41px] font-[PM] top-[5px]">
-                3.2
-              </Text>
-              <Text className="text-white text-[12px] top-[-5px] opacity-50 font-[PM]">
-                KM
-              </Text>
-              </View>
-              <View className=" -rotate-90" style={[shadowStyle, borderStyle]}>
-                <ProgressCircle progress={todaysGoal} size={150} strokeWidth={10} color="#0085FF" />
+            <View className="w-full flex justify-center items-center">
+              <View className="bg-[#10102C] mt-6 flex justify-center items-center w-full h-[300px] rounded-3xl border-[#20204C] border">
+                <View className=" w-full h-5/6 flex items-center justify-between">
+                  <Text className="text-white text-xl font-[PM]">
+                    Todays Goal
+                  </Text>
+                  <View className="relative flex justify-center items-center w-36 h-44">
+                    <View className=" bg-LB w-[120px] h-[120px] absolute rounded-full">
+                    </View>
+                  <View className="absolute flex justify-center items-center">
+                    <Text className="text-white text-[41px] font-[PM] top-[5px]">
+                      3.2
+                    </Text>
+                    <Text className="text-white text-[12px] top-[-5px] opacity-50 font-[PM]">
+                      KM
+                    </Text>
+                    </View>
+                    <View className=" -rotate-90" style={[shadowStyle, borderStyle]}>
+                      <ProgressCircle progress={todaysGoal} size={150} strokeWidth={10} color="#0085FF" />
+                    </View>
+                  </View>
+                  <Text className="text-white text-xl font-[PM]">
+                    ...
+                  </Text>
+                    <Button title="Increase" onPress={handlePress} />
+                </View>
               </View>
             </View>
-            <Text className="text-white text-xl font-[PM]">
-              ...
+            <Text className="text-white text-2xl mt-6 mb-2 font-[PM] font-semibold">
+              Challenges
             </Text>
-              <Button title="Increase" onPress={handlePress} />
+            {topChallenges.map((challenge) => (
+              <View key={challenge.title} className="flex justify-center h-14">
+                <View className="flex flex-row w-full justify-center">
+                  <SvgXml width={22} height={22} xml={challenge.type === 'speed' ? SpeedSvg : challenge.type === 'map' ? Map2Svg : challenge.type === "steps" ? StepsSvg : 'Unknown challenge'} className="fill-white"/>
+                  <Text className="text-white text-base font-[PM] ml-2 max-w-[82%] h-8 overflow-hidden">{challenge.title}</Text>
+                  <Text className="text-xl font-[PM] ml-auto text-[#0085FF]">{challenge.points}</Text>
+                </View>
+                <Progress.Bar className="w-full" color="#0085FF" unfilledColor="" borderWidth={0} progress={challenge.progress/challenge.goal} width={width*(11/12)} />
+              </View>
+            ))}
           </View>
+          {/* End of page 0 */}
+          </View>
+          <View className="w-full mt-20 flex items-center">
+            <View className="w-11/12">
+            <Text className="text-white text-3xl mb-6 font-[PM] font-semibold">
+              Challenges
+            </Text>
+            {allChallenges.map((challenge) => (
+              <View key={challenge.title} className="flex justify-center h-24">
+                <SvgXml width={22} height={22} xml={challenge.type === 'speed' ? SpeedSvg : challenge.type === 'map' ? Map2Svg : challenge.type === "steps" ? StepsSvg : 'Unknown challenge'} className="fill-white"/>
+                <View className="flex flex-row w-full justify-center">
+                  <Text className="text-white text-base font-[PM] ml-2 max-w-[82%] h-8 overflow-hidden">{challenge.title}</Text>
+                  <Text className="text-xl font-[PM] ml-auto text-[#0085FF]">{challenge.points}</Text>
+                </View>
+                <Progress.Bar className="w-full" color="#0085FF" unfilledColor="" borderWidth={0} progress={challenge.progress/challenge.goal} width={width*(11/12)} />
+              </View>
+            ))}
+            </View>
+          </View>
+          <View className="w-full mt-20 flex items-center">
+            <View className="w-11/12">
+              <Text className="text-white text-3xl mb-6 font-[PM] font-semibold">
+                Stats
+              </Text>
+            </View>
+          </View>
+          <View className="w-full mt-20 flex items-center">
+            <View className="w-11/12">
+              <Text className="text-white text-3xl mb-6 font-[PM] font-semibold">
+                Profile
+              </Text>
+            </View>
+          </View>
+        </Swiper>
+        <View className="w-full h-20 flex flex-row justify-around bg-[#10102C]">
+          <TouchableOpacity className="w-1/4 flex pt-4 items-center" onPress={() => swiperRef.current?.scrollTo(0)}>
+            <SvgXml width={24} height={24} xml={DashboardSvg} className={`fill-[#0085FF] opacity-${currentPageIndex == 0 ? "100" : "50" }`} />
+          </TouchableOpacity>
+          <TouchableOpacity className="w-1/4 flex pt-4 items-center" onPress={() => swiperRef.current?.scrollTo(1)}>
+            <SvgXml width={24} height={24} xml={StatsSvg} className={`fill-[#0085FF] opacity-${currentPageIndex == 1 ? "100" : "50" }`}/>
+          </TouchableOpacity>
+          <TouchableOpacity className="w-1/4 flex pt-4 items-center" onPress={() => swiperRef.current?.scrollTo(2)}>
+            <SvgXml width={24} height={24} xml={MapSvg} className={`fill-[#0085FF] opacity-${currentPageIndex == 2 ? "100" : "50" }`}/>
+          </TouchableOpacity>
+          <TouchableOpacity className="w-1/4 flex pt-4 items-center" onPress={() => swiperRef.current?.scrollTo(3)}>
+            <SvgXml width={24} height={24} xml={ProfileSvg} className={`fill-[#0085FF] opacity-${currentPageIndex == 3 ? "100" : "50" }`}/>
+          </TouchableOpacity>
+          
         </View>
-        </View>
-      </View>
     </View>
   )
 }
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    presets: ['babel-preset-expo', "module:metro-react-native-babel-preset"],
+    plugins: ["nativewind/babel", ["babel-plugin-inline-import",
+    {
+      "extensions": [".svg"]
+    }
+  ]], 
   };
 };
